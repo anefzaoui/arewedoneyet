1982e673bb8bc62e3b100d3760cb236b6249b79c
Ahmed Nefzaoui <nefzaoui.ahmed@hotmail.com>
2014-12-28 15:10:10 +0100

    Fetching Projects...

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,5 @@
 var awdy = {
-
-  loadJson: function(href, callback, bugparam) {
+  loadBugs: function(href, callback) {
     if (!callback)
       return;
     var xhr = new XMLHttpRequest();
@@ -8,27 +7,49 @@ var awdy = {
       console.error('Failed to fetch file: ' + href, xhr.statusText);
     };
     xhr.onload = function() {
-      if (bugparam) {
-        callback(JSON.parse(xhr.response.body.getElementsByTagName('pre')[0].innerHTML));
-      } else {
-        callback(xhr.response);
-      }
+      callback(JSON.parse(xhr.response.body.getElementsByTagName('pre')[0].innerHTML));
     };
-    xhr.open('GET', href); // async
+    xhr.open('GET', href, true); // async
     xhr.responseType = 'document';
     xhr.send();
   },
-  fetchProjectsInfo: function(){
-    this.loadJson("res/projects.json", function(obj){
-      console.log(JSON.parse(obj));
-    }, false);
-  }
 
+  loadProjects: function(href, callback) {
+    if (!callback)
+      return;
+    var xhr = new XMLHttpRequest();
+    xhr.onerror = function() {
+      console.error('Failed to fetch file: ' + href, xhr.statusText);
+    };
+    xhr.onload = function() {
+      callback(xhr.response);
+    };
+
+    xhr.open('GET', href); // async
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.responseType = 'json';
+    xhr.send();
+  },
+
+  addListeners: function(){
+
+  },
+
+  fetchProjectsInfo: function() {
+    var self = this;
+    function _fetch(obj) {
+      for(i=0; i<obj.length; i++) {
+        document.getElementById("projects").innerHTML += "<option id='"+obj[i].id+"'>"+obj[i].name+"</option>";
+      }
+      self.addListeners();
+    }
+    this.loadProjects("res/projects.json", _fetch);
+  }
 
 };
 
-window.addEventListener("load", function _init(evt){
-  window.removeEventListener("load",_init);
-  console.log("okay");
+window.addEventListener("load", function _init(evt) {
+  window.removeEventListener("load", _init);
   awdy.fetchProjectsInfo();
 });
